ab772a5660e9bb4c21298e512e332ebc738dff8d
EverSaul1 <ever.saul.2017@gmail.com>
2018-08-31 20:15:10 -0500

    dd

---
+++ vistas/modulos/matriculas.php
@@ -0,0 +1,92 @@
+<div class="content-wrapper">
+
+  <section class="content-header" >
+    
+    <h2 class="login-box-msg" style="font-family: sans-serif; font-weight: bold">
+      
+      
+       Bienvenido a Academico Kalbermatter
+    
+    </h2>
+
+     <h1 style="color:DodgerBlue; text-align: center">
+      
+      
+       DATOS DEL EDUCANDO PARA LA MATRICULA
+    
+    </h1>
+
+    <ol class="breadcrumb">
+      
+      <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+      
+      <li class="active">Matricula</li>
+    
+    </ol>
+
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+  <div class="box box-danger">
+
+   <div class="box-body">
+
+    <div class="row">
+
+      <div class="col-md-2">
+
+        <h5>DNI</h5>
+
+          <div class="input-group">
+
+                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+
+                <input type="number" class="form-control" placeholder="Ingrese dni">
+
+          </div>
+
+            <br>
+
+      </div>
+      <div class="col-md-2">
+
+        <h5>Codigo moludar</h5>
+
+          <div class="input-group">
+
+                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+
+                <input type="number" class="form-control" placeholder="codigo moludar">
+
+          </div>
+
+            <br>
+
+      </div>
+      <div class="col-md-2">
+
+        <h5>grupo</h5>
+
+          <div class="input-group">
+
+                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+
+                <input type="number" class="form-control" placeholder="grupo">
+
+          </div>
+
+            <br>
+
+      </div>
+
+      </div>
+    </div>
+
+   </div>
+
+    <!-- Default box -->
+
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
